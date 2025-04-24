6dbf0c52438295bede5378981c4827de180e5407
Asmaa Bahy <asmaaxbahy@gmail.com>
2025-04-24 13:56:54 +0100

    added motion for animation

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ export default function Title() {
   function handleClick() {}
   return (
     <section className="w-full p-3 flex flex-col items-center text-center py-10">
-      <h1 className="text-5xl xs:text-6xl xl:text-8xl mb-10 font-bold poetsen-one-regular text-white [text-shadow:_0_0_5px_#9B5FCA,_0_0_10px_#9B5FCA,_0_0_20px_#8230BA,_0_0_30px_#51107A] tracking-widest">
+      <h1 className="text-5xl xs:text-6xl xl:text-8xl mb-10 font-bold font-sans text-white tracking-widest">
         Asmaa Bahy
       </h1>
 
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 export default function RootLayout() {
   return (
@@ -7,12 +7,10 @@ export default function RootLayout() {
       <header className="sticky top-0 left-0 right-0 z-50 h-16 md:h-20 lg:h-20 3xl:h-32">
         <Navbar />
       </header>
-      <main className="text-pink-100">
+      <main className="text-center">
         <Outlet />
       </main>
-      <footer>
-
-      </footer>
+      <footer></footer>
     </>
   );
 }
